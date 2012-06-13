ab088f60d833fa2b1f071a91c309263c9ae7b1e8
Maik Scharlau <maik@clipflip.com>
2012-06-13 14:42:55 +0200

    Module Changed

--- lib/flowplayer/railties/generator/install_generator.rb
+++ lib/flowplayer/railties/generator/install_generator.rb
@@ -1,14 +1,13 @@
-module Flowplayer
-  module Generator
+Flowplayer::Generator
     class InstallGenerator < Rails::Generators::Base
       namespace 'flowplayer'
       source_root File.expand_path("../../../../../vendor/assets/", __FILE__)
       argument :install_type, :type => :string, :banner => "commercial", :required => false, :default => ''
-    
+      
       def install_flowplayer
         copy_file 'javascripts/flowplayer-3.2.4.min.js', Rails.root.join('public', 'javascripts', 'flowplayer.min.js')
       end
-    
+      
       def install_swfs
         if install_type == 'commercial'
           copy_file 'flash/flowplayer.commercial-3.2.5.swf', Rails.root.join('public', 'flowplayer.swf')
@@ -17,7 +16,7 @@ module Flowplayer
           copy_file '../../FLOWPLAYER_LICENSE.txt', Rails.root.join('FLOWPLAYER_LICENSE.txt')
         end
         copy_file 'flash/flowplayer.controls-3.2.3.swf', Rails.root.join('public', 'flowplayer.controls.swf')
+      end
+      
     end
-    
-  end
 end
